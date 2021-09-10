1385818cbf535f1cd4b8f13ada305ad6b73f3dcd
ADMIN <NoobieUs37@protonmail.com>
2021-09-10 19:38:35 +0530

    make perfect responsive

---
+++ build/precache-manifest.238ba0cf0a3b17b052aa30fc6b07dede.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "226556fb254567503c32ea972e5498a2",
+    "url": "/openweathermapapi/index.html"
+  },
+  {
+    "revision": "d7a035da3d0a8af2417f",
+    "url": "/openweathermapapi/static/js/2.2d265f19.chunk.js"
+  },
+  {
+    "revision": "48c904ac6633e15cf264",
+    "url": "/openweathermapapi/static/js/main.c63ebf44.chunk.js"
+  },
+  {
+    "revision": "64db938abb399e52b1db",
+    "url": "/openweathermapapi/static/js/runtime-main.bfb882fb.js"
+  }
+]);
